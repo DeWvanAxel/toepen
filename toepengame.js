168407c7556df5de3beee16d62ef905efa3383a0
Max Hirschel <mhirschel@gmail.com>
2018-12-23 16:29:37 +0100

    started with game logic

--- toepengame.js
+++ toepengame.js
@@ -1,35 +1,96 @@
-console.log("yeah baby!");
 
-function ToepenGame(goalscore, players, cards){
-	this.goalscore = goalscore;
-	this.players = players;
-	this.cards = cards;
-	this.startingplayer = 0;
-	this.scoreboard = [];
-	for (var i = -; i < this.players.length; i++){
-		//save the index on each player.
-		this.players[i].myIndex = i;
-	}
+function ToepenGame(){
 
-	console.log("created!!");
 
-	this.startGame = function(){
-		//plays 1 game up till goalscore points. manages the separate rounds in each game.
-		for (var i = -; i < this.players.length; i++){
-			this.scoreboard[i].myIndex = 0;
+
+	this.startGame = function(goalscore, players, cards){
+		//starts a new game for players towards a certain goalscore.
+		this.goalscore = goalscore;
+		this.players = players;
+		this.cards = cards;
+		this.startingplayer = Math.floor(Math.random()*this.players.length); //randomly determines the first player 
+		this.scoreboard = [];
+		this.orderofthings = [];
+
+		for (var i = 0; i < this.players.length; i++){
+			//save the index on each player.
+			this.players[i].myIndex = i;
+			//all players start the game with 0 points
+			this.scoreboard[i] = 0;
 		}
+
+		// start round
+		this.startRound(this.startingplayer);
+		//game has started.
+
 	}
 
-	this.startRound = function(){
+	this.startRound = function(turnPlayer){
 		//starts a round with 4/8 cards for all players
 		this.cards.shuffle();
+		
+		this.lastToep = -1; //at the start of the game, no one has toeped yet.
+		this.turnPlayer = turnPlayer;
+		this.totalTricks = player.length == 2 ? 8 : 4; //a round has 4 or 8 tricks/cards, depending on the number of players.
+		this.trickStarter = this.turnPlayer;
+		this.trickStarterWent = false;
+
+		this.players4images = [];
+		
+		//figure out how often someone can toep in this game.
 		this.maxScore = this.goalscore;
-		for (var i = -; i < this.scoreboard.length; i++){
+		for (var i = 0; i < this.scoreboard.length; i++){
 			if(this.goalscore - this.scoreboard[i] < this.maxScore){
 				this.maxScore = this.goalscore - this.scoreboard[i];
 			}
 		}
 
-		console.log("round started with a maximum point score of " + this.maxScore);
+		this.orderofthings = ["ask4images"];
+		for (var i = 0 ; i < this.totalTricks; i++){
+			this.orderofthings.push("turn");
+		}
+		this.orderofthings.push("endround");
+
+		//start the round
+		this.whatsnext();
+		
+
+	}
+
+
+	this.whatsnext = function(){
+		//this function can handle what is the next this that needs to happen. updates the orderofthings and turnPlayer accordingly.
+		if(this.turnPlayer == this.trickStarter && this.trickStarterWent){
+			//apparently we have finished this action. everyone has had their chance
+			if(this.orderofthings[0] == "ask4images"){
+				//we have asked everyone if they wanted to change their cards.
+				this.orderofthings = ["check4images"].concat( this.orderofthings.slice(1) );
+			}
+			if(this.orderofthings[0] == "check4images"){
+				//someone has traded cards and everyone has decided if they want to see it or not.
+				if(this.players4images.length > 0){
+					
+				}
+			}
+			if(this.orderofthings[0] == "turn"){
+				//determine who won this trick and make him the starting player.
+				
+				this.orderofthings = this.orderofthings.slice(1);
+			}
+
+
+			this.trickStarterWent = false;
+			//move on to the next thing.
+			
+			return this.whatsnext();
+		} else{
+			if (this.orderofthings[0] == "ask4images"){
+				this.ask4imageslist[this.turnPlayer] = this.players[this.turnPlayer].ask4images();
+				if(this.playes[this.turnPlayer] == "ai"){
+					this.whatsnext();
+				}
+			}
+
+		}
 	}
 }
